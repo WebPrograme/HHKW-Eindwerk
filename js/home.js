7de192f41066efdcbdc88a57052b0e1c6b5aa7f9
Yarne Gooris <yarne.gooris@gmail.com>
2025-06-17 16:31:16 +0200

    feat: Implement newsletter subscription with validation and success/error messages

--- js/home.js
+++ js/home.js
@@ -1,6 +1,44 @@
 import { getRequest, postRequest } from '../modules/Request.js';
 import { Articles } from '../modules/Components.js';
 
+// Newsletter subscription
+const newsletterSignupBtn = document.getElementById('newsletter-signup');
+if (newsletterSignupBtn) {
+	newsletterSignupBtn.addEventListener('click', async (e) => {
+		e.preventDefault();
+
+		const form = e.target.closest('.form-group');
+		const email = form.querySelector('input[name="email"]').value;
+
+		if (!email) {
+			form.querySelector('#newsletter-error p').innerHTML = 'Gebruik een geldig e-mailadres.';
+			form.querySelector('#newsletter-error').classList.add('show');
+			form.querySelector('#newsletter-success').classList.remove('show');
+			return;
+		}
+
+		try {
+			const response = await postRequest('/api/form/newsletter/subscribe', { email });
+			console.log('Response from newsletter subscription:', response);
+
+			form.querySelector('#newsletter-success p').innerHTML = 'Bedankt voor uw inschrijving!';
+			form.querySelector('#newsletter-success').classList.add('show');
+			form.querySelector('#newsletter-error').classList.remove('show');
+			form.querySelector('input').value = ''; // Clear the input field
+			form.querySelector('input').blur(); // Remove focus from the input field
+		} catch (error) {
+			if (error.status === 400) {
+				form.querySelector('#newsletter-error p').innerHTML = 'Gebruik een geldig e-mailadres.';
+			} else if (error.status === 401) {
+				form.querySelector('#newsletter-error p').innerHTML = 'Dit e-mailadres is al ingeschreven.';
+			}
+
+			form.querySelector('#newsletter-success').classList.remove('show');
+			form.querySelector('#newsletter-error').classList.add('show');
+		}
+	});
+}
+
 // Get recent 5 blog posts
 async function getBlogPosts() {
 	const posts = await getRequest('/api/blog/articles/all?page=0&count=5');
