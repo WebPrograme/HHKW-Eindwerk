f96f44d11724d3937768ec227a1da1b39ba225cb
Yarne Gooris <yarne.gooris@gmail.com>
2025-06-17 18:08:34 +0200

    fix: Update base URL to use production endpoint

--- modules/Request.js
+++ modules/Request.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://localhost:3000';
+const baseUrl = 'https://hhkw.onrender.com';
 
 async function postRequest(path, data, headers = { 'Content-Type': 'application/json' }, retry = false, trigger = null) {
 	if (trigger) trigger.classList.add('disabled');
